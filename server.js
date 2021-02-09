594417a308ea92afbdc5017c5cd28cb2bb4e1879
cskupie <Christian.skupien@daimler.com>
2021-02-09 11:43:29 +0100

    add some code

---
+++ server.js
@@ -0,0 +1,26 @@
+//imports
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+
+//global variables
+const port = 3000
+const app = express()
+
+projectData = {}
+
+//app configuration
+app.use(cors())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.use(express.static('website'))
+
+/*request-handler configurations
+ * Getters */
+
+app.get('/all', (req, res) => {
+    res.send('hello')
+})
+
+//start server
+app.listen(port, console.log(`Server started on port ${port}`))
---
+++ website/js/app.js
@@ -0,0 +1,62 @@
+//global Variables
+const API_KEY = '&appid=6ff9318fee80ae559827385ab850e862'
+const wheaterUrl = 'https://api.openweathermap.org/data/2.5/'
+
+//global elements
+const searchField = document.getElementById('search-field')
+const searchInput = document.getElementById('search-input')
+const section = document.querySelector('section')
+
+// helper funtions
+async function fetchFromWheater(endpoint, query) {
+    return await fetch(
+        `${wheaterUrl}${endpoint}?${query}&units=metric${API_KEY}`
+    )
+        .then((response) => response.json())
+        .catch((error) => console.error(error))
+}
+
+// https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
+
+// fetchFromWheater('weather', 'q=Oberkochen&units=metric').then((data) =>
+//     console.log(data.main.temp)
+// )
+
+searchInput.addEventListener('focus', () => searchField.classList.add('active'))
+searchInput.addEventListener('blur', (event) => {
+    if (!event.target.value) searchField.classList.remove('active')
+})
+
+function setData(data) {
+    searchField.classList.add('active')
+    searchInput.value = data.name
+    section.textContent = JSON.stringify(data)
+}
+
+function getEstimatedLocation() {
+    fetch('http://ip-api.com/json?fields=lat,lon')
+        .then((response) => response.json())
+        .then((data) =>
+            fetchFromWheater('weather', `lat=${data.lat}&lon=${data.lon}`)
+        )
+        .then(setData)
+}
+
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(
+            (data) =>
+                fetchFromWheater(
+                    'weather',
+                    `lat=${data.coords.latitude}&lon=${data.coords.longitude}`
+                ).then(setData),
+            () => fetchFromWheater('weather', `q=Stuttgart`).then(setData)
+        )
+    } else {
+        console.log('Geolocation is not supported by this browser.')
+        fetchFromWheater('weather', `q=Stuttgart`).then(setData)
+    }
+}
+getEstimatedLocation()
+
+getLocation()
